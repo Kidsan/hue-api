07e8983198048360fab720ab62d098458ff1f3bd
Kidsan <8798449+Kidsan@users.noreply.github.com>
2019-04-17 22:49:19 +0100

    add new authentication api

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 const express = require('express');
 const config = require('config');
 
+const Routes = require('./lib/routes/index.js');
+
 const appSettings = config.get('application');
+const routes = new Routes();
 
 const app = express();
 const { port } = appSettings;
 
 app.get('/', (req, res) => res.send('Get Out'));
+app.use('/api/', routes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
